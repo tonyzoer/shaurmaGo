42099289dab951a7ba0642ff224d76aa79d96daa
Okamanahi <okamanahi@gmail.com>
2017-04-18 13:48:23 +0300

    Relocate AssyncTask to inner class of Activity

--- app/src/main/java/com/example/zoer/shaurmago/ShaurmaMaps.java
+++ app/src/main/java/com/example/zoer/shaurmago/ShaurmaMaps.java
@@ -1,10 +1,10 @@
 package com.example.zoer.shaurmago;
 
-import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Pair;
 
-import com.example.zoer.shaurmago.tasks.PointGetter;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,8 +16,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.Console;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -62,5 +65,107 @@ public class ShaurmaMaps extends FragmentActivity implements OnMapReadyCallback 
     }
 
 
-}
+    class PointGetter extends AsyncTask<Void, Pair<LatLng,String>, JSONArray> {
+        private String urlGet = "https://shaurma-go-server-okamanahi.c9users.io/getallpoints.php";
+        GoogleMap mMap = null;
 
+        public PointGetter(GoogleMap googleMap) {
+            mMap = googleMap;
+        }
+
+        @Override
+        protected JSONArray doInBackground(Void... params) {
+
+            try {
+                JSONArray arr = new JSONArray(getJsonResponse(urlGet));
+
+                for (int i = 0; i < arr.length(); i++) {
+                    try {
+                        JSONObject obj = arr.getJSONObject(i);
+                        LatLng pos = new LatLng(obj.getDouble("Lat"),
+                                obj.getDouble("Lng"));
+                        publishProgress(new Pair<LatLng, String>(pos,obj.getString("name")));
+                    } catch (JSONException e) {
+
+                        e.printStackTrace();
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray jsonArray) {
+            super.onPostExecute(jsonArray);
+        }
+
+        @Override
+        protected void onProgressUpdate(Pair<LatLng,String>... values) {
+            for (Pair<LatLng,String> pair:values
+                 ) {
+                mMap.addMarker(new MarkerOptions().position(pair.first).title(pair.second));
+            }
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled(JSONArray jsonArray) {
+            super.onCancelled(jsonArray);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+        private String getJsonResponse(String urls) {
+            URL url = null;
+            try {
+                url = new URL(urls);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection conn = null;
+            String response = null;
+            try {
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                InputStream in = new BufferedInputStream(conn.getInputStream());
+                response = convertStreamToString(in);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        private String convertStreamToString(InputStream is) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append('\n');
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return sb.toString();
+        }
+    }
+}
