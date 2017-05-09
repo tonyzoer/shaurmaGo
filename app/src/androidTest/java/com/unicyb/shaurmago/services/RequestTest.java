3fea2a9930d71f866b6b345bdb1fbe0aecefcf96
Okamanahi <okamanahi@gmail.com>
2017-05-09 15:06:22 +0300

    change package name , add login activity

---
+++ app/src/androidTest/java/com/unicyb/shaurmago/services/RequestTest.java
@@ -0,0 +1,21 @@
+package com.unicyb.shaurmago.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Zoer on 04.05.2017.
+ */
+public class RequestTest {
+    @Test
+    public void post() throws Exception {
+
+    }
+
+    @Test
+    public void hashMapToUrl() throws Exception {
+
+    }
+
+}
